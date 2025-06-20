3ec5dea8f39b48710faea72bd15e077f5e84dbda
Attayab-31 <attayabpc2@gmail.com>
2025-06-20 07:01:40 -0700

    Initial commit

--- src/pages/ChatbotPage.jsx
+++ src/pages/ChatbotPage.jsx
@@ -593,7 +593,7 @@ const ChatbotPage = () => {
               </Button>
             </div>
             <p className="text-xs text-gray-500 mt-2 text-center">
-              Service URL: {HUGGINGFACE_SPACE_URL}
+              Service URL: <p>personality predictor</p>
             </p>
           </div>
         </div>
